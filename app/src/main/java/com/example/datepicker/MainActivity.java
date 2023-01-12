0b26cffb79427183114377cddd61f2a8657e31a0
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2023-01-12 23:25:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.datepicker;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.MessageFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn);
+        TextView textView = findViewById(R.id.tv);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        textView.setText(MessageFormat.format("{0}/{1}/{2}", String.valueOf(i2), String.valueOf(i1 + 1), String.valueOf(i)));
+                    }
+                }, year, month, day);
+                dialog.show();
+            }
+        });
+    }
+}
